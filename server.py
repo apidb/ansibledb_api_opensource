469491b3a3cb1799628a2e54eff707abd3b259ef
TobyLockyer <tobylockyer@users.noreply.github.com>
2021-01-05 17:02:21 +0000

    Initial commit

---
+++ server.py
@@ -0,0 +1,34 @@
+from pymongo import MongoClient
+from flask import Flask, request
+
+import json
+
+app = Flask(__name__)
+
+client = MongoClient("localhost", 27017)
+db = client['ansibledb']
+servers = db.servers
+
+@app.route('/api/setup')
+def setup():
+    servers.create_index("ansible_fqdn")
+    return servers.index_information()
+
+@app.route('/api/ansiblefacts', methods=['POST'])
+def ansiblefacts():
+    if request.method == 'POST':
+        content = json.loads(request.data)
+        fqdn =  content["ansible_facts"]["ansible_fqdn"]
+        servers.update({"ansible_fqdn":fqdn}, {"$set": content }, True)
+    return content
+
+
+@app.route('/api/servers', methods=['GET'])
+def ansibleservers():
+    cursor = servers.find({}, {'_id': False})
+    list_result = list(cursor)
+    result = json.dumps(list_result,default=str)
+    return result
+
+if __name__ == '__main__':
+    app.run()
